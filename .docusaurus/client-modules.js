1d37e2fb18c63da852d681586fd8e809d21b5480
Sergey Kuzmin <s.v.kuzmin@example.com>
2025-06-06 12:07:21 +0300

    Remove build/cache folders from repo; add to .gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/s.v.kuzmin/.nvm/versions/node/v22.16.0/lib/node_modules/@unic/autodoc-cli/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/s.v.kuzmin/.nvm/versions/node/v22.16.0/lib/node_modules/@unic/autodoc-cli/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/s.v.kuzmin/.nvm/versions/node/v22.16.0/lib/node_modules/@unic/autodoc-cli/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/s.v.kuzmin/.nvm/versions/node/v22.16.0/lib/node_modules/@unic/autodoc-cli/src/css/custom.css"),
-];
